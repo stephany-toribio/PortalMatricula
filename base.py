fe71c4d5fc9b6649ff2f8e6d94e5c38506f9d8c0
stephany-toribio <stephany06toribio@gmail.com>
2024-05-05 16:02:21 -0500

    Visualizar plan de estudios grafo

--- base.py
+++ base.py
@@ -10,27 +10,57 @@ def grafo_curso(curso):
     data = cargarData()
     G = nx.DiGraph()
 
-    codigo = data.loc[data["Nombre"] == curso,"Código"].values[0]
-    G.add_node(codigo)
+    stack = [curso]
+    while stack:
+        curso_actual = stack.pop()
+        codigo = data.loc[data["Nombre"] == curso_actual,"Código"].values[0]
+        G.add_node(codigo)
 
-    flag = False
-    while flag == False:
-        if (data["Requisito"] == codigo).any():
-            descendiente_nombre = data.loc[data["Requisito"] == codigo, "Nombre"].values[0]
-            descendiente_codigo = data.loc[data["Requisito"] == codigo, "Código"].values[0]
+        requisitos = data.loc[data["Requisito"] == codigo, "Nombre"].values[:]
+        for descendiente_nombre in requisitos:
+            descendiente_codigo = data.loc[data["Nombre"] == descendiente_nombre, "Código"].values[0]
             G.add_node(descendiente_codigo)
             G.add_edge(codigo, descendiente_codigo)
-            codigo = descendiente_codigo
-        else:
-            nx.draw_networkx(G)
-            flag = True
+            stack.append(descendiente_nombre)
 
-print(grafo_curso("Procesos de innovación en ingeniería"))
+    return nx.draw_networkx(G)
 
-"""
+def grafo_planEstudio():
+    data = cargarData()
+    G = nx.DiGraph()
+
+    ciclos = sorted((data["Ciclo"].unique()))
+    cursos =data["Nombre"].unique()
+    for curso in cursos:
+        stack = [curso]
+        while stack:
+            curso_actual = stack.pop()
+            codigo = data.loc[data["Nombre"] == curso_actual,"Código"].values[0]
+            G.add_node(codigo)
+            
+            requisitos = data.loc[data["Requisito"] == codigo, "Nombre"].values[:]
+            #requisitos = [valor for valor in data.loc[data["Requisito"] == codigo, "Nombre"].values[:] if valor not in ["40 créditos", "80 créditos", "120 créditos", "150 créditos","C0090 /150 créditos"]]
+            for descendiente_nombre in requisitos:
+                descendiente_codigo = data.loc[data["Nombre"] == descendiente_nombre, "Código"].values[0]
+                G.add_node(descendiente_codigo)
+                G.add_edge(codigo, descendiente_codigo)
+                stack.append(descendiente_nombre)
+
+    pos = {}
+    vertical_spacing = 100
+
+    for i, ciclo in enumerate(ciclos):
+        cursos_ciclo = data[data["Ciclo"] == ciclo]["Código"].tolist()
+        for j, curso in enumerate(cursos_ciclo):
+            pos[curso] = (j, -i)
+
+    nx.draw(G, pos, with_labels=True, node_size=500, node_color="skyblue", font_size=10, arrowsize=20)
+    plt.title("Plan de Estudios")
+    plt.show()           
+
 def main():
-    return "yes"
+    #grafo_curso("Estadística y probabilidades")
+    grafo_planEstudio()
 
 if __name__ == "__main__":
     main()
-"""
